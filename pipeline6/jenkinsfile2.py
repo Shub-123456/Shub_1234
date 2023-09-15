ed1c354684afb0ec7898818eadd4ff9141ee1ac4
Shub-123456 <141324911+Shub-123456@users.noreply.github.com>
2023-09-15 15:58:45 +0530

    Update jenkinsfile2.py

--- pipeline6/jenkinsfile2.py
+++ pipeline6/jenkinsfile2.py
@@ -1,49 +1,23 @@
 import boto3
 import json
-iam = boto3.client('iam')
+
+user = boto3.client('iam')
 def create_policy():
-    policy_name = "EC2Permissions-ProfileSummary"
-    policy_document = {
-
-        "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Sid": "Statement1",
-                    "Effect": "Allow",
-                    "Action": [
-                        "ec2:*",
-                        "iam:GetAccountSummary",
-                        "iam:GetLoginProfile",
-                        "iam:GetUser"
-                    ],
-                "Resource": "arn:aws:ec2:::*",
-                "Resource": "arn:aws:iam:::*"
-            }
-        ]
-
-    }
-
-    iam_client = boto3.client("iam")
-
-    response = iam_client.create_policy(
-        PolicyName=policy_name,
-        PolicyDocument=json.dumps(policy_document)
+    with open('pipeline6/jenkinsfile1.json', 'r') as f:
+        policy_document = json.load(f)
+    response = user.create_policy(
+        PolicyName="abc",
+        PolicyDocument=json.dumps(policy_document) 
     )
-
-    print("Policy created:")
-    print(response)
-
 def create_user():
-    response = iam.create_user(
+    response = user.create_user(
         UserName='bob'
     )
-    print(response)
-
+# create_user()
 def attach_policy():
-    response = iam.attach_user_policy(
-    UserName='bob',
-    PolicyArn='arn:aws:iam::430912479781:policy/EC2Permissions-ProfileSummary'
+    response = user.attach_user_policy(
+    UserName= 'bob',
+    PolicyArn='arn:aws:iam::190616427825:policy/All_Policy'
 )
-create_policy()
-create_user()
-attach_policy()
+# attach_policy()
+# create_policy()
