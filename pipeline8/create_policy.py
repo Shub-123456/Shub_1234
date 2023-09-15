83e790366c9dc03d87cc0770efce1a48871c43e9
Shub-123456 <141324911+Shub-123456@users.noreply.github.com>
2023-09-15 16:21:48 +0530

    Update create_policy.py

--- pipeline8/create_policy.py
+++ pipeline8/create_policy.py
@@ -1,12 +1,12 @@
 import boto3
 import json
 
-user = boto3.client('iam')
+iam = boto3.client('iam')
 def create_policy():
     with open('pipeline8/policy.json', 'r') as f:
         policy_document = json.load(f)
     response = user.create_policy(
-        PolicyName="123",
+        PolicyName="abc",
         PolicyDocument=json.dumps(policy_document) 
     )
 def create_user():
