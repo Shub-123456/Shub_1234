77b6c42210836e53bf9213e6551333a2af753434
Shub-123456 <141324911+Shub-123456@users.noreply.github.com>
2023-09-19 18:46:30 +0530

    Create map.py

---
+++ pipeline7/map_policy/map.py
@@ -0,0 +1,10 @@
+import boto3
+import json
+
+user = boto3.client('iam')
+def attach_policy():
+    response = user.attach_user_policy(
+    UserName= 'tiger',
+    PolicyArn='arn:aws:iam::655523803653:policy/abc'
+)
+attach_policy()
