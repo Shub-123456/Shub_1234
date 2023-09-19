ee6409326fb21abf4c0635cda640ad17ddaf645a
Shub-123456 <141324911+Shub-123456@users.noreply.github.com>
2023-09-19 18:48:29 +0530

    Update create_policy.py

--- pipeline8/create_policy.py
+++ pipeline8/create_policy.py
@@ -2,22 +2,16 @@ import boto3
 import json
 
 user = boto3.client('iam')
-def create_policy():
-    with open('pipeline8/policy.json', 'r') as f:
-        policy_document = json.load(f)
-    response = iam.create_policy(
-        PolicyName="abcd",
-        PolicyDocument=json.dumps(policy_document) 
-    )
+
 def create_user():
     response = user.create_user(
-        UserName='pathan'
+        UserName='tiger'
     )
 create_user()
 def attach_policy():
     response = user.attach_user_policy(
-    UserName= 'pathan',
+    UserName= 'tiger',
     PolicyArn='arn:aws:iam::655523803653:policy/abc'
 )
 attach_policy()
-# create_policy()
+
