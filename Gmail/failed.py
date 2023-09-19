9b64c905531b7d4b705e01b7aec6b79e635a70fa
Shub-123456 <141324911+Shub-123456@users.noreply.github.com>
2023-09-19 23:57:07 +0530

    Create failed.py

---
+++ Gmail/failed.py
@@ -0,0 +1,28 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# Gmail account details
+gmail_user = 'shubham.chide@gmail.com' # Your Gmail address
+gmail_app_password = 'Shub_1234' # Use the App Password generated earlier
+# Email details
+to_email = 'chakarnikanth@gmail.com' # Recipient's email address
+subject = 'Shubham Chide'
+message = 'Pipeline Failed.'
+# Create a MIMEText object with your message
+msg = MIMEMultipart()
+msg['From'] = gmail_user
+msg['To'] = to_email
+msg['Subject'] = subject
+msg.attach(MIMEText(message, 'plain'))
+# Connect to Gmail's SMTP server
+try:
+  server = smtplib.SMTP('smtp.elasticemail.com', 2525)
+  server.starttls()
+  server.login(gmail_user, gmail_app_password)
+  # Send the email
+  server.sendmail(gmail_user, to_email, msg.as_string())
+  print('Email sent successfully!')
+except Exception as e:
+  print(f'Error: {str(e)}')
+finally:
+  server.quit()++++..................................................
